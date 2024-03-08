4927d7a107f2e020ab00aaaee9c3dbff1541e4d7
PedroLorenzen <christianplorenzen@outlook.com>
2024-03-08 10:48:23 +0100

    adding components for html

--- ses5/DogInder/app.js
+++ ses5/DogInder/app.js
@@ -3,21 +3,39 @@ import express from 'express';
 const app = express();
 app.use(express.static('public')); 
 // vi bruger express.static for at vise vores statiske filer til brugeren
+// app.use static gør at brugeren udelukkende har adgang til en enkel side så de ikke kan gå ind og se ting de ikke må/har brug for.
+
 
 import path from 'path';
 import getMatches from './util/matches.js';
+
+import fs from "fs";
+import { send } from 'process';
+const homepage = fs.readFileSync("./public/pages/homepage/homepage.html").toString();
+console.log(homepage);
+
+const header = fs.readFileSync("./public/components/header/header.html").toString();
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
 
 //getMatches();
 
 // ================================ HTML =============================== //
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve("public/homepage/homepage.html"));
+    //res.sendFile(path.resolve("public/pages/homepage/homepage.html"));
+    res.send(homepage);
 });
 
 app.get('/matches', (req, res) => {
-    res.sendFile
-    (path.resolve("public/matches/matches.html"));
+    res.sendFile(path.resolve("public/pages/matches/matches.html"));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve("public/pages/contact/contact.html"));
+});
+
+app.get('/page', (req, res) => {
+    res.sendFile(path.resolve("public/pages/contact/contact.html"));
 });
 
 // ================================ API =============================== //
