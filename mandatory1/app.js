34d6e4ddf313ccb7d6f2108be0a8fa84768ed230
PedroLorenzen <christianplorenzen@outlook.com>
2024-03-21 12:53:05 +0100

    remove comment

--- mandatory1/app.js
+++ mandatory1/app.js
@@ -4,7 +4,7 @@ import pagesRouter from './routers/pagesRouter.js'
 const app = express()
 app.use(express.static('public'))
 
-app.use(pagesRouter) // indeholder nu alle routes til pages
+app.use(pagesRouter)
 
 const PORT = 3000
 app.listen(PORT, () => console.log('Server is running on port:', PORT))
