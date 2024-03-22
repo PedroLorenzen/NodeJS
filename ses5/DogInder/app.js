1ef2556051fb8b744c4dee05237ff9aa9775bbc8
PedroLorenzen <christianplorenzen@outlook.com>
2024-03-22 10:13:25 +0100

    working on contact api route

--- ses5/DogInder/app.js
+++ ses5/DogInder/app.js
@@ -3,8 +3,10 @@ import express from 'express';
 const app = express();
 app.use(express.static('public')); 
 
-const port = process.env.PORT;
-console.log(`Your port is ${port}`);
+app.use(express.urlencoded({ extended: true })); // middleware for at parse body fra POST requests
+
+const PORT = process.env.PORT;
+console.log(`Your port is ${PORT}`);
 
 // vi bruger express.static for at vise vores statiske filer til brugeren
 // app.use static gør at brugeren udelukkende har adgang til en enkel side
@@ -17,5 +19,5 @@ app.use(pagesRouter); // indeholder nu alle routes til pages
 app.use(contactRouter); // indeholder nu alle routes til contact
 
 // Her bruger vi en synlig port hvilket ikke er anbefalet. Så vi definerer istedet miljøvariablerne gennem vores script i package.json
-const PORT = 3000;
+//const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on port:`, PORT))
--- ses5/DogInder/routers/contactRouter.js
+++ ses5/DogInder/routers/contactRouter.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 const contactRouter = Router();
 
 contactRouter.post("/api/contact", (req, res) => {
-    console.log(toString(req.body));
+    console.log("Contact form submitted", req.body);
     res.send("Contact form submitted");
 });
-
+// console.log giver mig: Contact form submitted { name: 'Anders', email: 'chri@gmail.com', message: 'asdjasdkl' }
 export default contactRouter;
