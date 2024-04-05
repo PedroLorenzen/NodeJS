756cc8117615ad0365f6e880e837181de7212a75
PedroLorenzen <christianplorenzen@outlook.com>
2024-04-05 11:43:15 +0200

    Working with Store, Parent, Child and props

---
+++ ses8/vite-Family/src/components/Home/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Fridge from "../Fridge/Fridge.svelte";
+
+</script>
+
+<Fridge />
+
+<slot></slot>
+
+<p>|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    |</p>
+    <p>&nbsp;&nbsp;&nbsp;Home sweet home </p>
+<p>|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    |</p>
+
