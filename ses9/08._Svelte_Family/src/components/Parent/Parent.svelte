40c10977110cb1194231473b0bcd06326c1c077b
PedroLorenzen <christianplorenzen@outlook.com>
2024-04-20 21:14:28 +0200

    Making security and authentication for node js application

---
+++ ses9/08._Svelte_Family/src/components/Parent/Parent.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let parentName;
+    export let myChildren;
+
+    import { fridgeMessages } from "../../stores/fridgeMessageStore.js";
+        
+    import Child from "../Child/Child.svelte";
+
+    function handleShowLove(childName) {
+        console.log(`${parentName} is loved by ${childName} veeeeery much.`);
+    }
+
+    let treasureChest = ["🔱", "🏅", "⭐️", "🔑", "🏆", "👑", "🌟"];
+
+    function handleTakeFromTreasureChest() {
+        treasureChest.pop();
+        treasureChest = treasureChest;
+    }
+
+    function wipeFridge() {
+        fridgeMessages.set([{ message: "Svelte Family Fridge" }]);
+    }
+
+    // assignment give parents the power to wipe the fridge
+</script>
+
+<h1>{parentName}</h1>
+
+<button on:click={wipeFridge}>Wipe the fridge</button>
+
+<p>
+{#each treasureChest as treasure}
+    {treasure + " "}
+{/each}
+</p>
+
+{#each myChildren as child}
+    <Child child={child} onShowLove={handleShowLove} 
+        onTakeFromTreasureChest={handleTakeFromTreasureChest} />
+{/each}
