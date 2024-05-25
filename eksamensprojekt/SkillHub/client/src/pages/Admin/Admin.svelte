9396f3969cb268d130bb2a06c6d2985216feb286
PedroLorenzen <christianplorenzen@outlook.com>
2024-05-25 19:51:37 +0200

    Finished styling table now making edit and delete jobs

--- eksamensprojekt/SkillHub/client/src/pages/Admin/Admin.svelte
+++ eksamensprojekt/SkillHub/client/src/pages/Admin/Admin.svelte
@@ -121,6 +121,27 @@
             },
         );
     }
+
+    async function getJobs() {
+        try {
+            const response = await fetch("http://localhost:8080/jobs", {
+                credentials: "include",
+            });
+            if (response.status === 429) {
+                navigate("/RateLimitExceeded");
+                throw new Error("Rate limit exceeded");
+            } else if (!response.ok) {
+                throw new Error(
+                    "Failed to fetch jobs: " + (await response.text()),
+                );
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching jobs:", error);
+            toast.error("Error fetching jobs: " + error.message);
+        }
+    }
 </script>
 
 <Toaster />
@@ -137,7 +158,8 @@
                         <th>Email</th>
                         <th>Location</th>
                         <th>Admin</th>
-                        <th>Actions</th>
+                        <th>Update</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -183,30 +205,64 @@
 
 <style>
     main {
-        padding: 100px;
+        background-color: white;
+        width: 100%;
+        padding: 0 30px 50px 30px;
+        margin-top: 25px;
+        margin-left: -30px;
+        margin-right: 50px;
     }
     .table-container {
         max-height: 60vh; /* Adjust the height as needed */
         overflow: auto;
-        margin-top: 20px;
-        border: 1px solid #ddd;
+        width: 80%;
+        margin: 0 auto;
     }
     table {
+        margin-bottom: 20px;
+        padding: 20px;
+        color: black;
+        background-color: lightgray;
+        border-radius: 8px;
         width: 100%;
-        border-collapse: collapse;
-        margin-top: 20px;
     }
-    th,
     td {
         border: 1px solid #ddd;
-        padding: 8px;
+        border-radius: 10px;
+        background-color: #28a745;
+
+    }
+    td input {
+        margin: 5px;
+        background-color: #ccc;
+        color: black;
+        border: 1px solid #ccc;
+        padding: 5px 10px 5px 10px;
+        box-shadow: 8px 8px 8px rgba(0, 0, 0, 0.3);
+        border-radius: 10px;
+        font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+            "Lucida Sans", Arial, sans-serif;
     }
     th {
-        background-color: #f2f2f2;
-        text-align: left;
+        background-color: #007bff;
+        text-align: center;
+        color: black;
+        border: 1px solid #ccc;
+        padding: 5px 10px;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+        border-radius: 10px;
+        font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+            "Lucida Sans", Arial, sans-serif;
     }
     button {
-        padding: 5px 10px;
-        cursor: pointer;
+        margin: 5px;
+        background-color: #ccc;
+        color: black;
+        border: 1px solid #ccc;
+        padding: 5px 10px 5px 10px;
+        box-shadow: 8px 8px 8px rgba(0, 0, 0, 0.3);
+        border-radius: 10px;
+        font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+            "Lucida Sans", Arial, sans-serif;
     }
 </style>
